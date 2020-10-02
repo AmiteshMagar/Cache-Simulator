b6eba1f646585ce810b2a74ffbe44540c4682779
Amitesh Magar <amitesh.magar@gmail.com>
2020-10-03 00:51:42 +0530

    trial now includes file stream code for reading from a file. Trial2 created which holds the logic of how to get around tricky strings that have multiple spaces.

--- trial.cpp
+++ trial.cpp
@@ -1,29 +1,61 @@
 #include<iostream>
-#include<sstream>
+#include<fstream>
 
 using namespace std;
 
-int main(int argc, char* argv[]){
-	
-	string temp = argv[1];
-	stringstream Cache_lines(argv[3]);
-	int cl = 0;
-	Cache_lines >> cl;
-	
-	if(temp.compare("LRU") == 0)
-	{
-		cout << "LRU" << endl;
-	}
-	else if (temp.compare("FIFO") == 0)
-	{
-		cout << "FIFO" << endl;
-	}
-	else if (temp.compare("OPTIMAL") == 0)
-	{
-		cout << "OPTIMAL" << endl;
-	}
-	else
-		cout << "Incorrect arguments entered" << endl;
-	return 0;
+int main(){
+	//fstream fio;
 
+	ifstream infile;
+
+	string line="";
+
+	//fio.open("sample.txt", ios::app | ios::in);
+	infile.open("sample.txt");
+
+	cout << "Reading from the file" << endl << endl;
+
+	while(infile){
+		getline(infile, line);
+		cout << line << endl;
+		if (infile.eof())
+			break;
+	}
+
+	infile.close();
+
+	// ios::in reads a file for reading
+	// ios:: our opens a file for writing into it
+	// ios::app opens a file and appends all the inputs at its end, it also does not delete presaved text
+	// ios::ate opens the file and moves the control to the end of the file
+	// ios::trunc removes the data that already exists in a file
+
+	//Entering data to be stored into the file 
+
+	//Step for recording OUTPUT.
+/*
+	while(fio){
+		getline(cin, line);
+		if(line == "-1")
+			break;
+
+		fio << line << endl;
+	}*/
+
+	//Reading data from a given file. We still use the getline() feature
+
+	//Step for reading INPUT
+
+/*
+	fio.seekg(0, ios::beg);
+
+	while(fio){
+		getline(fio, line);
+
+		cout << line << endl;
+	}
+
+	fio.close();
+*/
+	return 0;
 }
---
+++ trial2.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+#include<sstream>
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+	
+	ifstream InFile;
+	InFile.open("sample.txt");
+	string str ;
+
+	while(InFile){
+		getline(InFile, str);
+		istringstream ss(str);
+		do{
+			string word;
+			ss >> word;
+			if(word != "" && word !=" ")
+				cout << word << endl;
+		}while(ss);
+		if(InFile.eof())
+			break;
+	}
+
+	//String stream method
+	/*
+	do{
+		string word;
+		ss >> word;
+		if(word != ""){
+		
+			//Cache manipulation code goes here
+
+		}
+	}while(ss);*/
+
+
+	/*--Normal method--/
+	string word = "";
+	char check;
+	
+	for(int i=0; i<len; i++){
+		check = str[i];
+		
+		if(check != ' ')
+			word = word + check;
+		else if(check == ' ' || check == '\0'){
+			cout << word << endl;
+			word = "";
+		}
+	}*/
+
+/*
+	str.erase(remove(str.begin(),str.end(),' '), str.end());
+	char ch = str[1];
+	cout << str <<endl;
+	cout << ch << endl;*/
+
+	return 0;
+}
